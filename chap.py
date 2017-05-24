f65e60fd3d211885e8975739329c62f98b688191
lilyz@Feng-PC <you@example.com>
2017-05-24 15:40:54 +0800

    zach

--- chap.py
+++ chap.py
@@ -13,38 +13,6 @@ import random
 import string
 Pass = "'result': 'p'"
 Fail = "'result': 'f'"
-def ssh_connChap():
-    import paramiko
-    import time
-    server = '10.84.2.164'
-    uname = 'administrator'
-    pwd = 'password'
-    #server = '10.84.2.100'
-
-    data = ''
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(server, username=uname, password=pwd)
-    c = ssh.invoke_shell()
-
-    c.setblocking(1)
-    resp = b''
-
-    while not c.recv_ready():
-        resp = c.recv(2000)
-        if resp.decode().endswith('@cli> '):
-            break
-    # if c.recv_ready():
-    #     resp = c.recv(9999)
-
-        # data+=resp
-        data = 'administrator@cli> '
-
-    # only @cli, send command to console to execute
-
-    if resp.decode().endswith('@cli> '):
-
-        return c,ssh
 def chapSend_cmd(c, cmdstr,password):
     data = ''
     if cmdstr.endswith('\n'):
