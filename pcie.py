326771c6166d22321ef39204c8b93cb7f01ceae9
zach@zach_PC <zach.feng@cn.promise.com>
2017-07-03 17:28:41 +0800

    zach

--- pcie.py
+++ pcie.py
@@ -10,6 +10,11 @@ Fail = "'result': 'f'"
 def verifyPcie(c):
     FailFlag = False
     tolog("<b>Verify pcie </b>")
+    result = SendCmd(c, 'pcie')
+
+    if result.count('CtrlId: 2') != 2:
+        FailFlag = True
+        tolog('<font color="red">Fail: pcie </font>')
 
     if FailFlag:
         tolog('\n<font color="red">Fail: Verify pcie </font>')
@@ -21,6 +26,11 @@ def verifyPcie(c):
 def verifyPcielist(c):
     FailFlag = False
     tolog("<b>Verify pcie -a list </b>")
+    result = SendCmd(c, 'pcie -a list')
+
+    if result.count('CtrlId: 2') != 2:
+        FailFlag = True
+        tolog('<font color="red">Fail: pcie -a list </font>')
 
     if FailFlag:
         tolog('\n<font color="red">Fail: Verify pcie -a list </font>')
@@ -33,19 +43,96 @@ def verifyPcieInvalidOption(c):
     FailFlag = False
     tolog("<b>Verify pcie invalid option</b>")
     command = ['pcie -x', 'pcie -a list -x']
+
     for com in command:
         tolog('<b> Verify ' + com + '</b>')
         result = SendCmd(c, com)
         if "Error (" not in result or "Invalid option" not in result:
             FailFlag = True
             tolog('\n<font color="red">Fail: ' + com + ' </font>')
+
     if FailFlag:
         tolog('\n<font color="red">Fail: Verify pcie invalid option </font>')
         tolog(Fail)
     else:
         tolog('\n<font color="green">Pass</font>')
         tolog(Pass)
+
 def verifyPcieInvalidParameters(c):
+    FailFlag = False
+    tolog("<b>Verify pcie invalid parameters</b>")
+    command = ['pcie test', 'pcie -a test']
+
+    for com in command:
+        tolog('<b> Verify ' + com + '</b>')
+        result = SendCmd(c, com)
+        if "Error (" not in result or "Invalid setting parameters" not in result:
+            FailFlag = True
+            tolog('\n<font color="red">Fail: ' + com + ' </font>')
+
+    if FailFlag:
+        tolog('\n<font color="red">Fail: Verify pcie invalid parameters </font>')
+        tolog(Fail)
+    else:
+        tolog('\n<font color="green">Pass</font>')
+        tolog(Pass)
+
+def verifyPcieMissingParameters(c):
+    FailFlag = False
+    tolog("<b>Verify pcie missing parameters</b>")
+    command = ['pcie -a ']
+
+    for com in command:
+        tolog('<b> Verify ' + com + '</b>')
+        result = SendCmd(c, com)
+        if "Error (" not in result or "Missing parameter" not in result:
+            FailFlag = True
+            tolog('\n<font color="red">Fail: ' + com + ' </font>')
+
+    if FailFlag:
+        tolog('\n<font color="red">Fail: Verify pcie missing parameters </font>')
+        tolog(Fail)
+    else:
+        tolog('\n<font color="green">Pass</font>')
+        tolog(Pass)
+
+
+def bvt_verifyPcie(c):
+    FailFlag = False
+    tolog("<b>Verify pcie </b>")
+    result = SendCmd(c, 'pcie')
+
+    if result.count('CtrlId: 2') != 2:
+        FailFlag = True
+        tolog('<font color="red">Fail: pcie </font>')
+
+    return FailFlag
+
+def bvt_verifyPcielist(c):
+    FailFlag = False
+    tolog("<b>Verify pcie -a list </b>")
+    result = SendCmd(c, 'pcie -a list')
+
+    if result.count('CtrlId: 2') != 2:
+        FailFlag = True
+        tolog('<font color="red">Fail: pcie -a list </font>')
+
+    return FailFlag
+
+def bvt_verifyPcieInvalidOption(c):
+    FailFlag = False
+    tolog("<b>Verify pcie invalid option</b>")
+    command = ['pcie -x', 'pcie -a list -x']
+    for com in command:
+        tolog('<b> Verify ' + com + '</b>')
+        result = SendCmd(c, com)
+        if "Error (" not in result or "Invalid option" not in result:
+            FailFlag = True
+            tolog('\n<font color="red">Fail: ' + com + ' </font>')
+
+    return FailFlag
+
+def bvt_verifyPcieInvalidParameters(c):
     FailFlag = False
     tolog("<b>Verify pcie invalid parameters</b>")
     command = ['pcie test', 'pcie -a test']
@@ -55,13 +142,10 @@ def verifyPcieInvalidParameters(c):
         if "Error (" not in result or "Invalid setting parameters" not in result:
             FailFlag = True
             tolog('\n<font color="red">Fail: ' + com + ' </font>')
-    if FailFlag:
-        tolog('\n<font color="red">Fail: Verify pcie invalid parameters </font>')
-        tolog(Fail)
-    else:
-        tolog('\n<font color="green">Pass</font>')
-        tolog(Pass)
-def verifyPcieMissingParameters(c):
+
+    return FailFlag
+
+def bvt_verifyPcieMissingParameters(c):
     FailFlag = False
     tolog("<b>Verify pcie missing parameters</b>")
     command = ['pcie -a ']
@@ -71,12 +155,8 @@ def verifyPcieMissingParameters(c):
         if "Error (" not in result or "Missing parameter" not in result:
             FailFlag = True
             tolog('\n<font color="red">Fail: ' + com + ' </font>')
-    if FailFlag:
-        tolog('\n<font color="red">Fail: Verify pcie missing parameters </font>')
-        tolog(Fail)
-    else:
-        tolog('\n<font color="green">Pass</font>')
-        tolog(Pass)
+
+    return FailFlag
 
 if __name__ == "__main__":
     start = time.clock()
