bc934f47fad4d6634bda0f34cad06b885d276c68
jacky <jacky@bing.com>
2017-08-14 15:07:08 +0800

    pool test

--- pool.py
+++ pool.py
@@ -2654,7 +2654,7 @@ if __name__ == "__main__":
     # print infodictret(c, "phydrv", "", 1)
     # print infodictret(c, "volume", "", 1)
     # print infodictret(c, "pool", "", 1)
-    poolcreateandlist(c,1)
+    #poolcreateandlist(c,1)
     #poolmodifyandlist(c)
     #poolmodifyandlist(c)
     # poolcreateandlist(c,poolnum)
@@ -2664,16 +2664,19 @@ if __name__ == "__main__":
 
     # pool name is renamed and extend with other available disks
     #poolmodifyandlist(c)
-
-    volumecreateandlist(c, 10)
+    print infodictret(c,"phydrv","",1)
+    print infodictret(c, "pool", "", 1)
+    print infodictret(c, "ctrl", "", 2)
+    print infodictret(c, "bgasched", "", 1)
+    #volumecreateandlist(c, 10)
     # volumecreateandlist(c,volnum)
     # create 3 volumes for each pool
 
-    snapshotcreateandlist(c,2)
+    #snapshotcreateandlist(c,2)
     # snapshotcreateandlist(c,snapshotnum)
     # create snapshotnum snapshots for each volume
     #SendCmd(c,"snapshot")
-    clonecreateandlist(c, 2)
+    #clonecreateandlist(c, 2)
     # clonecreateandlist(c,clonenum)
     # create clonenum for each snapshot
     #exportunexport(c, "volume")
